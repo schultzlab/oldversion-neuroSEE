4ccefb57f21441c311c07c13c4bca21c6da72496
Seig <s.prado17@imperial.ac.uk>
2018-09-14 13:49:10 +0100

    modified the inputs to neuroSEE_nnd.m

--- dependencies/neuroSEE_nnd.m
+++ dependencies/neuroSEE_nnd.m
@@ -14,16 +14,17 @@
 %
 %   INPUT:
 %       R: ratiometric time series whose size is [cells x samples]
+%       thr: threshold value (usually 0.022)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function spikes = neuroSEE_nnd(R)
+function spikes = neuroSEE_nnd(R, thr)
 
 % Normalise the Ca traces first
 nR = bsxfun(@rdivide, bsxfun(@minus, R, min(R,[],2)),(max(R,[],2)-min(R,[],2)));
 nC = size(nR,1);
 mAlength = 5;
-thr = 0.022; %0.009
+%thr = 0.022; %0.009
 spikes = zeros(size(nR));
 for ri=1:nC
     nRs = conv(nR(ri,:),ones(mAlength,1)/mAlength,'same');
