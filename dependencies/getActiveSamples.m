1c9bec1dfdd4f87654532fac52b7644718126844
Ann Go <m.go@imperial.ac.uk>
2019-03-15 14:41:26 +0000

    deleted seigfreds old files

--- dependencies/getActiveSamples.m
+++
@@ -1,5 +0,0 @@
-function active = getActiveSamples(tracking,thrV)
-    vx = diff(tracking(:,1));
-    vy = diff(tracking(:,2));
-    active = sqrt(vx.^2+vy.^2)>thrV;
-end
--- dependencies/parsave.m
+++
@@ -1,3 +0,0 @@
-function parsave(x,y,fname)
-save(fname, 'x', 'y','-v7.3')
-end
